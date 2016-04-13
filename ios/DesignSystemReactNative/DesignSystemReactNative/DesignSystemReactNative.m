a3132a4f22e18b998fcde0dfb0b7889bafb67f41
ibogdanov <ibogdanov@salesforce.com>
2016-04-12 20:43:40 -0700

    added static lib

---
+++ ios/DesignSystemReactNative/DesignSystemReactNative/DesignSystemReactNative.m
@@ -0,0 +1,13 @@
+//
+//  DesignSystemReactNative.m
+//  DesignSystemReactNative
+//
+//  Created by Ivan Bogdanov on 4/12/16.
+//  Copyright © 2016 Salesforce. All rights reserved.
+//
+
+#import "DesignSystemReactNative.h"
+
+@implementation DesignSystemReactNative
+
+@end
