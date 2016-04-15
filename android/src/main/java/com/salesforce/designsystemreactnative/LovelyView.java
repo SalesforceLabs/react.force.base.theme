c068c71ef319f3e4ed90829bc319167a3fb32225
ibogdanov <ibogdanov@salesforce.com>
2016-04-15 12:41:02 -0700

    progress

--- android/src/main/java/com/salesforce/designsystemreactnative/LovelyView.java
+++ android/src/main/java/com/salesforce/designsystemreactnative/LovelyView.java
@@ -6,12 +6,14 @@ import android.graphics.drawable.ShapeDrawable;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Color;
 import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class LovelyView extends View{
     private ShapeDrawable mDrawable;
+    private Paint paint;
     //label text
     public LovelyView(Context context){
         super(context,null);
@@ -20,13 +22,21 @@ public class LovelyView extends View{
         int width = 300;
         int height = 50;
 
+        paint = new Paint();
+        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
         mDrawable = new ShapeDrawable(new OvalShape());
         mDrawable.getPaint().setColor(0xff74AC23);
-        mDrawable.setBounds(x, y, x + width, y + height);
     }
     @Override
     protected void onDraw(Canvas canvas) {
+        int size = 100;
+        mDrawable.setBounds(0, 0, this.getWidth(), this.getHeight());
         mDrawable.draw(canvas);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(size);
+        canvas.drawText("@",0,size, paint);
+//        super.onDraw(canvas);
+
     }
 
 }
