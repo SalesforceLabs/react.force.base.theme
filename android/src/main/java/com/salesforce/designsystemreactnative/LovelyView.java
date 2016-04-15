c3f1988be5bf2aab9e4d5b1ee7b7e7a6c6f38fe4
ibogdanov <ibogdanov@salesforce.com>
2016-04-15 12:02:45 -0700

    progress

--- android/src/main/java/com/salesforce/designsystemreactnative/LovelyView.java
+++ android/src/main/java/com/salesforce/designsystemreactnative/LovelyView.java
@@ -1,69 +1,32 @@
 package com.salesforce.designsystemreactnative;
 
 import android.content.Context;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.ShapeDrawable;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.View;
-/**
- * Created by ibogdanov on 4/14/16.
- */
+
 public class LovelyView extends View{
-    private int circleCol, labelCol;
+    private ShapeDrawable mDrawable;
     //label text
-    private String circleText;
-    //paint for drawing custom view
-    private Paint circlePaint;
     public LovelyView(Context context){
-        super(context);
-        //paint object for drawing in onDraw
-        circlePaint = new Paint();
-        //get the attributes specified in attrs.xml using the name we included
-/*
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
-                R.styleable.LovelyView, 0, 0);
-        try {
-            //get the text and colors specified using the names in attrs.xml
-            circleText = a.getString(R.styleable.LovelyView_circleLabel);
-            circleCol = a.getInteger(R.styleable.LovelyView_circleColor, 0);//0 is default
-            labelCol = a.getInteger(R.styleable.LovelyView_labelColor, 0);
-        } finally {
-            a.recycle();
-        }
-*/
+        super(context,null);
+        int x = 10;
+        int y = 10;
+        int width = 300;
+        int height = 50;
+
+        mDrawable = new ShapeDrawable(new OvalShape());
+        mDrawable.getPaint().setColor(0xff74AC23);
+        mDrawable.setBounds(x, y, x + width, y + height);
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        //draw the View
-        //get half of the width and height as we are working with a circle
-        int viewWidthHalf = this.getMeasuredWidth()/2;
-        int viewHeightHalf = this.getMeasuredHeight()/2;
-        //get the radius as half of the width or height, whichever is smaller
-//subtract ten so that it has some space around it
-        int radius = 0;
-        if(viewWidthHalf>viewHeightHalf)
-            radius=viewHeightHalf-10;
-        else
-            radius=viewWidthHalf-10;
-
-        circlePaint.setStyle(Style.FILL);
-        circlePaint.setAntiAlias(true);
-
-        //set the paint color using the circle color specified
-        circlePaint.setColor(circleCol);
-
-        canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, circlePaint);
-
-        //set the text color using the color specified
-        circlePaint.setColor(labelCol);
-
-        //set text properties
-        circlePaint.setTextAlign(Paint.Align.CENTER);
-        circlePaint.setTextSize(50);
-
-        //draw the text using the string attribute and chosen properties
-        canvas.drawText(circleText, viewWidthHalf, viewHeightHalf, circlePaint);
+        mDrawable.draw(canvas);
     }
+
 }
