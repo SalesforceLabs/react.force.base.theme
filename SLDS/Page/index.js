ebd3ed41a5baaae6055fc2d4115d2de481eb4955
ibogdanov <ibogdanov@salesforce.com>
2016-05-05 11:24:16 -0700

    page component

---
+++ SLDS/Page/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const React = require('react-native');
+const View = React.View;
+
+const styles = {
+  contaiter:{
+    postion:'absolute',
+    top:0,
+    bottom:0,
+    left:0,
+    right:0
+  }
+};
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        {this.props.children}
+      </View>
+    );
+  }
+});
