de723a5d1b3f87d06a6b38b027d36e3c731ea7c2
ibogdanov <ibogdanov@salesforce.com>
2016-05-19 21:17:13 -0700

    image property

--- SLDS/Tiles/List/index.js
+++ SLDS/Tiles/List/index.js
@@ -10,13 +10,20 @@ module.exports = React.createClass({
   getDefaultProps: function() {
     return {
       title:'',
-      detail:''
+      detail:'',
+      image:null
     };
   },
   setNativeProps(nativeProps) {
     this._root.setNativeProps(nativeProps);
   },
-  render: function() {
+  getIcon(){
+    if(this.props.image){
+      return this.props.image
+    }
+    return <Icons.Standard style={styles['View slds-icon icon-doctype-zip']} name={this.props.icon} />
+  },
+  render() {
     return (
       <View ref={component => this._root = component} {...this.props} style={styles['View slds-list__item slds-not-selected']}>
         <View style={[styles['View slds-media slds-tile'],{
@@ -24,7 +31,10 @@ module.exports = React.createClass({
             flexDirection:'row'}]}>
 
           <View style={styles['View slds-media__figure']}>
+{/*
             <Icons.Standard style={styles['View slds-icon icon-doctype-zip']} name={this.props.icon} />
+*/}
+            { this.getIcon() }
           </View>
 
           <View style={styles['View slds-media__body']}>
