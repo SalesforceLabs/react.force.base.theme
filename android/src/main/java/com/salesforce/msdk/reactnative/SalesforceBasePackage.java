4381d03dd4e2a2fb27b2123b16977d9aeb811df4
Alexander Luksidadi <alexander.luksidadi@gmail.com>
2016-11-21 23:54:48 -0500

    Fixed package name typo

--- android/src/main/java/com/salesforce/msdk/reactnative/SalesforceBasePackage.java
+++ android/src/main/java/com/salesforce/msdk/reactnative/SalesforceBasePackage.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.salesforce.masdk.reactnative;
+package com.salesforce.msdk.reactnative;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
